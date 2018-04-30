fb06e65b0d5917098379417bd6bfd3d461fa4c09
Michal Ciebiada <michal.ciebiada@u2i.com>
2018-04-30 23:27:11 +0200

    Downgrade adapter to websql as idb doesn't work on ios :(

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ PouchDB.plugin(PouchDBUpsert)
 const dbHost = process.env.REACT_APP_DB_HOST
 
 const remotedb = new PouchDB(`${dbHost}/db`)
-const localdb = new PouchDB('local')
+const localdb = new PouchDB('local', { adapter: 'websql' })
 
 export function signUp(login, password) {
   return remotedb.signUp(login, password)
