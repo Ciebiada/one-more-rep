8ffc514edebfd5205bfc3a56ea0516bd952c74a9
Michal Ciebiada <ciebiada@gmail.com>
2018-05-28 23:08:30 +0200

    Hide stats

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -22,7 +22,6 @@ class Hero extends Component {
             <div className='container'>
               <div className='navbar-brand'>
                 <Link className='navbar-item' to='/'>One More Rep</Link>
-                <Link className='navbar-item' to='/stats'><strong>Stats</strong></Link>
                 <span className='navbar-burger burger' onClick={this.toggleMenu}>
                   <span></span>
                   <span></span>
